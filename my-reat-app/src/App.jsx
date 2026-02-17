d1f23066b8e8b91396f467b0cfc628d7c3008ca3
Khamardarshit <khamarrdarshir@gmail.com>
2026-02-17 17:14:30 +0000

    done

--- my-reat-app/src/App.jsx
+++ my-reat-app/src/App.jsx
@@ -7,11 +7,11 @@ function App() {
 
   return (
     <>
-      {/* <welcome /> */}
-      {/* <welcoome /> */}
+      <welcome />
+      <welcoome />
       <student studentname="darshit"/>
     </>
   )
 }
 
-export default welcome;
+export default student;
